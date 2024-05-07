9e00718f894d48cdd594a834d3ad53f7b37f9e6b
Steven <memora182@soy.sena.edu.co>
2024-05-07 17:31:52 -0500

    Se agrego codigo ak security

--- src/main/java/Config/securityConfig.java
+++ src/main/java/Config/securityConfig.java
@@ -1,14 +1,17 @@
 package Config;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.catalina.authenticator.SpnegoAuthenticator.AuthenticateAction;
 import org.hibernate.mapping.Array;
-import org.hibernate.mapping.List;
+import org.springframework.security.core.userdetails.User;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+// import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -18,6 +21,7 @@ import org.springframework.security.config.annotation.authentication.configurers
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -25,6 +29,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.NoOpAccessDeniedHandler;
+
 
 @Configuration
 @EnableWebSecurity
@@ -34,7 +39,14 @@ public class securityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception {
 
-        return httpSecurity.build();
+        return httpSecurity
+        .csrf(csrf -> csrf.disable())
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(http -> {
+            http.requestMatchers(HttpMethod.GET , "/auth/hello" ).permitAll();
+            http.requestMatchers(HttpMethod.GET , "/auth/hello-secured" ).hasAuthority("Read"); // 49:35
+        })
+        .build();
     }
 
     @Bean
@@ -53,25 +65,22 @@ public class securityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService () {
-        List<Userdetails> userDatailsList = new ArrayList<>(); 
+    public UserDetailsService userDetailsService() {
+        List<UserDetails> userDetailsList = new ArrayList<>(); 
 
-        userDetailsList  userDetails = User.withUsername
-        ("Santiago")
-        .password("1234")
-        .roles("Admin")
-        .authorities("Read" , "Create")
-        .build();
+        userDetailsList.add(User.withUsername("Santiago")
+            .password("1234")
+            .roles("Admin")
+            .authorities("Read", "Create")
+            .build());
 
         userDetailsList.add(User.withUsername("Andres")
-        .password("2111")
-        .roles("Usuario")
-        .authorities("Read")
-        .build());
+            .password("2111")
+            .roles("Usuario")
+            .authorities("Read")
+            .build());
 
-    
         return new InMemoryUserDetailsManager(userDetailsList);
-        
     }
 
     @Bean
